03a7f1537d9622bda83ddc873953c05ec5b78388
Andy Dirnberger <andy.dirnberger@gmail.com>
2023-11-03 20:27:33 -0400

    Find longest common prefix when renaming files
    This simplifies the process of renaming bin and cue files. I will no
    longer have to specify the prefix for the files; the application will
    figure it out for me.

--- src/utils.rs
+++ src/utils.rs
@@ -36,3 +36,78 @@ pub fn find_files(root: PathBuf, extensions: Vec<String>) -> Vec<PathBuf> {
 
     files_found
 }
+
+// Adapted from https://users.rust-lang.org/t/is-this-code-idiomatic/51798/2.
+pub fn longest_common_prefix(vals: &[String]) -> &str {
+    if vals.is_empty() {
+        return "";
+    }
+
+    let common = &vals[0];
+
+    for (i, c) in common.chars().enumerate() {
+        for val in vals {
+            if val.chars().nth(i) != Some(c) {
+                return &common[..i];
+            }
+        }
+    }
+
+    common
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_longest_common_prefix_with_no_strings() {
+        assert_eq!(longest_common_prefix(&[]), "");
+    }
+
+    #[test]
+    fn test_longest_common_prefix_with_one_string() {
+        assert_eq!(longest_common_prefix(&["abc".to_string()]), "abc");
+    }
+
+    #[test]
+    fn test_longest_common_prefix_with_one_string_twice() {
+        assert_eq!(
+            longest_common_prefix(&["abc".to_string(), "abc".to_string()]),
+            "abc"
+        );
+    }
+
+    #[test]
+    fn test_longest_common_prefix_with_one_string_that_is_a_substring_of_the_other() {
+        assert_eq!(
+            longest_common_prefix(&["abc".to_string(), "abcdef".to_string()]),
+            "abc"
+        );
+        assert_eq!(
+            longest_common_prefix(&["abcdef".to_string(), "abc".to_string()]),
+            "abc"
+        );
+    }
+
+    #[test]
+    fn test_longest_common_prefix_with_no_common_prefix() {
+        assert_eq!(
+            longest_common_prefix(&["abc".to_string(), "def".to_string()]),
+            ""
+        );
+    }
+
+    #[test]
+    fn test_longest_common_prefix_with_some_strings() {
+        assert_eq!(
+            longest_common_prefix(&[
+                "abcdef".to_string(),
+                "abcdeg".to_string(),
+                "abcdhi".to_string(),
+                "abcjkl".to_string(),
+            ]),
+            "abc"
+        );
+    }
+}
